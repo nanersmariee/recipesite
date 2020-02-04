e647587da846c38631173a670a903f73d382b2ee
nanersmariee <anna.marie19@gmail.com>
2020-02-04 05:06:01 +0000

    created and started templates for base and homepage

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from flask import Flask, redirect, request, render_template, session
+from flask import Flask, redirect, request, render_template, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import Strict Undefined
 
 
 app = Flask(__name__)
-app.jinja_env.undefined = StrictUndefined
-app.jinja_env.auto_reload = True
+# app.jinja_env.undefined = StrictUndefined
+# app.jinja_env.auto_reload = True
 
 #Required to use Flask sessions and the debug toolbar
 app.secret_key = "adobo"
 
+@app.route('/')
+def begin_homepage():
+    """Homepage"""
+
+    return render_template
